06ebf81a8bc22a069e74cafb38625c5a049391c8
Uukow <93025470+Uukow@users.noreply.github.com>
2024-05-30 10:21:56 +0300

    first commit

---
+++ View/footer.php
@@ -0,0 +1,26 @@
+
+                <!-- Footer Start -->
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6">
+                            <script>document.write(new Date().getFullYear())</script> © MWN 
+                            </div>
+                            <div class="col-md-6">
+                                <div class="text-md-end footer-links d-none d-md-block">
+                                    <a href="javascript: void(0);">Developed By : Abdulkadir Uukow</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+                <!-- end Footer -->
+
+            </div>
+
+            <!-- ============================================================== -->
+            <!-- End Page content -->
+            <!-- ============================================================== -->
+
+        </div>
+        <!-- END wrapper -->
